60db57a62936d7226a6aa6416696647ba6d4d348
Kyle <florez.photo@gmail.com>
2021-04-06 17:29:01 -0500

    gi

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+fs.writeFile(fileName, data, (err) =>
+
+    err ? console.error(err) : console.log('No Errors! Write To File Complete...')
+
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+}
 
 // Function call to initialize app
 init();
