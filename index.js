3339f3e30b78f64310702cb6642d2ff8ee6356de
Kyle <florez.photo@gmail.com>
2021-04-06 17:45:19 -0500

    added questions

--- index.js
+++ index.js
@@ -7,20 +7,73 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
 
+{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title'
+},
+
+{
+    type: 'input', 
+    message: 'Please give a short summary of your project',
+    name: 'description'
+},
+
+{
+    type: 'input',
+    message: 'What technologies were used?',
+    name: 'tech'
+},
+
+{
+    type: 'input',
+    message: 'Please provide installation instructions',
+    name: 'install' 
+},
+
+{
+    type: 'input',
+    message: 'Please describe the usage of your project.',
+    name: 'usage'
+},
 
 
+{
+    type: 'list',
+    message: 'Please select the type of License you would like to use...',
+    name: 'license',
+    choices: ['MIT', 'ISC', 'Artistic', 'No License'],
+},
 
 
+{
+    type: 'input',
+    message: 'Please add Contributors / Contribution Guidelines',
+    name: 'contributions'
+},
+
+{
+    type: 'input',
+    message: 'Please add test guidelines',
+    name: 'test'
+},
 
 
+{
+    type: 'input',
+    message: 'Please enter your e-mail address',
+    name: 'email'
+},
 
 
+{
+    type: 'input',
+    message: 'Please enter your your GitHub username...',
+    name: 'username'
+},
 
 
-
-
-
-
+    
 ];
 
 // TODO: Create a function to write README file
@@ -37,7 +90,6 @@ fs.writeFile(fileName, data, (err) =>
 function init() {
 
 
-}
 
 // Function call to initialize app
 init();
